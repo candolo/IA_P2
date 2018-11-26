d9a8b2d09566c947b4ae34025079870fed5df77c
candolo <xixa94@live.com.pt>
2018-11-26 16:48:37 +0000

    Node

--- BN.py
+++ BN.py
@@ -9,12 +9,16 @@ Created on Mon Oct 15 15:51:49 2018
 
 class Node():
     def __init__(self, prob, parents = []):
-        pass
+        self.parents = parents
+        self.prob = prob
     
     def computeProb(self, evid):
-        pass
-
-        return 0
+        if len(self.parents) == 0:
+            return [1-self.prob[0],self.prob[0]]
+        elif len(self.parents) == 1:
+            return [1-self.prob[evid[self.parents[0]]], self.prob[evid[self.parents[0]]]]
+        elif len(self.parents) == 2:
+            return [1-self.prob[evid[self.parents[0]],evid[self.parents[1]]],self.prob[evid[self.parents[0]],evid[self.parents[1]]]]
     
 class BN():
     def __init__(self, gra, prob):

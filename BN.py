0c16b49ccdf959b88e10c2d0a7af3e7e510397cf
candolo <xixa94@live.com.pt>
2018-11-28 18:59:10 +0000

    Compute Posterior

--- BN.py
+++ BN.py
@@ -5,6 +5,7 @@ Created on Mon Oct 15 15:51:49 2018
 @author: mlopes
 """
 
+import copy
 
 
 class Node():
@@ -26,16 +27,38 @@ class BN():
         self.prob = prob
 
     def computePostProb(self, evid):
-        pass
-               
-        return 0
+        if len(self.graph[evid.index(-1)]) == 1 and evid[self.graph[evid.index(-1)][0]] in (0,1):
+            return self.prob[evid.index(-1)].prob[evid[self.graph[evid.index(-1)][0]]]
+        else:
+            var_pos = evid.index(-1)
+            var_pos_empty = []
+            var_equals_1 = 0
+            var_equals_1_and_0 = 0
+            var_pos = evid.index(-1)
+            
+            for k in range(len(evid)):
+                if evid[k] == []:
+                    var_pos_empty.append(k)
+                    
+            ev = list(evid)
+            
+            for i in range(2):
+                ev[var_pos] = i
+                for j in range(2):
+                    ev[var_pos_empty[0]] = j
+                    for m in range(2):
+                        ev[var_pos_empty[1]] = m
+                        mult = 1
+                        for l in range(len(evid)):
+                            mult = mult * self.prob[l].computeProb(tuple(ev))[ev[l]]
+                        if i == 1:
+                            var_equals_1 += mult
+                        var_equals_1_and_0 += mult
+            return var_equals_1/var_equals_1_and_0
         
         
     def computeJointProb(self, evid):
         joint = 1
         for i in range(len(evid)):
-            if evid[i] == 0:
-                joint = joint * (self.prob[i].computeProb(evid)[0])
-            else:
-                joint = joint * (self.prob[i].computeProb(evid)[1])
+                joint = joint * (self.prob[i].computeProb(evid)[evid[i]])
         return joint

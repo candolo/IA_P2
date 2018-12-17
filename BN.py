d19bb29fd9d98cb8c3e05ffef62f50ab8e5469cb
candolo <32075711+candolo@users.noreply.github.com>
2018-12-17 14:18:13 +0000

    test

--- BN.py
+++ BN.py
@@ -4,6 +4,7 @@ Created on Mon Oct 15 15:51:49 2018
 
 @author: mlopes
 """
+#test
 
 import numpy as np
 from itertools import product
@@ -12,15 +13,15 @@ class Node():
     def __init__(self, prob, parents = []):
         self.parents = parents
         self.prob = prob
-    
+
     def computeProb(self, evid):
         parents_ev = []
-        
+
         for i in self.parents:
             parents_ev.append(evid[i])
-        
+
         return [1-self.prob[tuple(parents_ev)],self.prob[tuple(parents_ev)]]
-    
+
 class BN():
     def __init__(self, gra, prob):
         self.graph = gra
@@ -30,9 +31,9 @@ class BN():
         var_pos = evid.index(-1)
         var_true = 0
         var_true_and_false = 0
-        
+
         combs = list(product([0,1], repeat=len(evid)))
-        
+
         valid = True
         for t in combs:
             for i in range(len(t)):
@@ -46,8 +47,8 @@ class BN():
             valid = True
 
         return var_true/var_true_and_false
-        
-        
+
+
     def computeJointProb(self, evid):
         joint = 1
         for i in range(len(evid)):
